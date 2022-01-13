1ffb0db7ff64c615aec051724013903a886bfa11
chrisstina <chrisstina@gmail.com>
2022-01-13 16:33:42 +0200

    remove 'INTERNATIONAL' document type
    for the moment Mixvel supports only passport and certificate of birth

--- dist/mixvel/mappers/dictionary/documentType.js
+++ dist/mixvel/mappers/dictionary/documentType.js
@@ -5,7 +5,6 @@ var documentType_1 = require("../../constants/documentType");
 function toMixvel(docType) {
     switch (docType) {
         case "PASSPORT":
-        case "INTERNATIONAL":
             return documentType_1.MixvelDocumentType.PASSPORT;
         case "BIRTHDAY_CERTIFICATE":
             return documentType_1.MixvelDocumentType.BIRTHDAY_CERTIFICATE;
--- src/mixvel/mappers/dictionary/documentType.ts
+++ src/mixvel/mappers/dictionary/documentType.ts
@@ -4,7 +4,6 @@ import {MixvelDocumentType} from "../../constants/documentType";
 export function toMixvel(docType: DocumentType): MixvelDocumentType {
     switch (docType) {
         case "PASSPORT":
-        case "INTERNATIONAL":
             return MixvelDocumentType.PASSPORT
         case "BIRTHDAY_CERTIFICATE":
             return MixvelDocumentType.BIRTHDAY_CERTIFICATE
