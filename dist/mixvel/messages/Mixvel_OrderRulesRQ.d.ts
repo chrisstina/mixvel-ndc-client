da2d413586aae28071d922642859344399cfac26
chrisstina <chrisstina@gmail.com>
2022-01-17 09:46:48 +0200

    add missing dist files

---
+++ dist/mixvel/messages/Mixvel_OrderRulesRQ.d.ts
@@ -0,0 +1,9 @@
+import { GenericNDCMessage } from "./GenericNDCMessage";
+export declare class Mixvel_OrderRulesRQ implements GenericNDCMessage {
+    get xmlns(): {
+        "xmlns:m": string;
+    };
+    get nodeName(): string;
+    RulesCoreRequest: {};
+    constructor(offerId: string, offerItemIds: string[]);
+}
