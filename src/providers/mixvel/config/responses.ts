fd42c021400f225c8d23b8970224692aff3d669e
chrisstina <chrisstina@gmail.com>
2022-02-04 14:43:57 +0200

    add mixvel auth:AuthResponse node to allowed

--- src/providers/mixvel/config/responses.ts
+++ src/providers/mixvel/config/responses.ts
@@ -1,5 +1,6 @@
 export const allowedNodeNames = [
     "Auth:AuthResponse",
+    "auth:AuthResponse",
     "Shop:Mixvel_AirShoppingRS",
     "Services:Mixvel_ServiceListRS",
     "Op:Mixvel_OfferPriceRS",
